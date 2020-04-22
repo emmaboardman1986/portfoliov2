6b2b7f34f16c0bdad3e33e278a86e8aadc86ab4a
E2 <e2@macbook-pro-5.home>
2020-04-22 14:45:01 +0100

    fix safari ios calc bug

--- src/components/header/NavLinkList.js
+++ src/components/header/NavLinkList.js
@@ -15,9 +15,7 @@ const NavLinkListWrapper = styled.ul`
   background-color: ${colors.brandWhite};
   display: flex;
   flex-direction: column;
-  /* fallback height for safari */
-  height: 90vh;
-  height: calc(100vh - 70px);
+  height: calc(100% - 70px);
   position: fixed;
   z-index: 200;
   bottom: 0;
